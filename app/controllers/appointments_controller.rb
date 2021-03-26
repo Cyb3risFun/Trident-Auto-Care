db7dc0fb7be6114b69d0546f58c7b7ca2f1a3834
sleepLazy <armine.armine712@gmail.com>
2021-03-26 22:51:08 +0800

    appointment add, cancel, edit

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,8 +1,11 @@
 class AppointmentsController < ApplicationController
+	before_action :find_apt, only: [:show, :edit, :update, :destroy]
 	def index
+		@appointments=Appointment.all.order("created_at DESC")
 	end
 
 	def show
+		
 	end
 
 	def new
@@ -22,16 +25,24 @@ class AppointmentsController < ApplicationController
 	end
 
 	def update
+		if @appointment.update(apt_params)
+			redirect_to @appointment
+		else
+			render 'edit'
+		end
 	end
 
-	def cancel
+	def destroy
+		@appointment.destroy
+		redirect_to root_path
 	end
 
 	private
 	def find_apt
+		@appointment=Appointment.find(params[:id])
 	end
 
 	def apt_params
-		params.require(:appointment).permit(:service, :time)
+		params.require(:appointment).permit(:service, :time, :vehicle_vin, :vehicle_name, :vehicle_manufacture, :vehicle_type, :vehicle_year, :vehicle_license_plate_num)
 	end
 end
