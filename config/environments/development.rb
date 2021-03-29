2ed839d9b35e637f02ca81df87a899b1c7169ccd
sleepLazy <armine.armine712@gmail.com>
2021-03-30 02:40:56 +0800

    password recover

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,8 +71,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "tridentcare1234@gmail.com",
+    password: "group2@123"
+  }
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.raise_delivery_errors = true
 
 end
